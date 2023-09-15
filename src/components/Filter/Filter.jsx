267ebd549984c6e93474c0d50d94770077b61814
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-09-15 14:49:47 +0300

    final

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,7 +3,12 @@ import { FindForm } from 'components/Phonebook.styled';
 export const Filter = ({ onSearch }) => {
   return (
     <FindForm>
-      <input type="text"  onChange ={(e)=>{onSearch(e)}} />
+      <input
+        type="text"
+        onChange={e => {
+          onSearch(e);
+        }}
+      />
       Find contacts by name
     </FindForm>
   );
