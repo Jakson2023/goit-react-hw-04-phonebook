36a82d37b38bec855dbaab13e1d2ee0c00cca9cb
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-09-15 14:36:50 +0300

    without-local

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,10 @@
+import { FindForm } from 'components/Phonebook.styled';
+
+export const Filter = ({ onSearch }) => {
+  return (
+    <FindForm>
+      <input type="text"  onChange ={(e)=>{onSearch(e)}} />
+      Find contacts by name
+    </FindForm>
+  );
+};
